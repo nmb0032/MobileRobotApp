c35c43fe0d427bc0b0f6cbd27ae72f9dae7249a1
Chrissybell <christobelmackgeek@gmail.com>
2022-01-23 18:54:08 -0600

    OpenCV canny edge and activity separation

--- app/src/main/java/com/example/robovision/MainActivity.java
+++ app/src/main/java/com/example/robovision/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.robovision;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.opencv.android.OpenCVLoader;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Creating OpenCV instance
-        if (!OpenCVLoader.initDebug())
-            Log.e("OpenCV", "unable to load OpenCV");
-        else
-            Log.d("OpenCV", "OpenCV opened successfully");
+        /* switch to new activity start */
+        b1 = findViewById(R.id.page1);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,OpenCVActivity.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+        /* switch to new activity end */
 
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null) {
