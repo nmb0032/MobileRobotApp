c36e1bdc1ce6122fe16ea4cffac012b8c7aeffb9
Yuantai Pan <tiger.stevesyd.pan@gmail.com>
2022-01-26 11:45:21 -0600

    Created remote control activity.

--- app/src/main/java/com/example/robovision/MainActivity.java
+++ app/src/main/java/com/example/robovision/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.robovision;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.opencv.android.OpenCVLoader;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRCActivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +22,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Assigning GUI variables initialized above
+        mRCActivityBtn = (Button)findViewById(R.id.remote_btn);
+
         //Creating OpenCV instance
         if (!OpenCVLoader.initDebug())
             Log.e("OpenCV", "unable to load OpenCV");
         else
             Log.d("OpenCV", "OpenCV opened successfully");
 
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(bluetoothAdapter == null) {
-            //bluetooth unsupported
-            Log.e("Bluetooth","Bluetooth not supported");
-        }
-        else {
-            Log.d("Bluetooth", "Bluetooth adapter initiated");
-        }
+        //Onclick listeners
+
+        mRCActivityBtn.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Switches to bluetooth activity on button press
+             */
+            @Override
+            public void onClick(View v){
+                openRemoteActivity();
+            }
+        });
+
+    }
+
+    private void openRemoteActivity(){
+        Intent intent = new Intent(this, RemoteControl.class);
+        startActivity(intent);
     }
 }
