b3e069666ef6845298bae8f9ddc0fedc32f349de
nmb0032 <nmb0032@auburn.edu>
2022-02-09 10:32:43 -0600

    initial commit of color blob detection OpenCV

--- app/src/main/java/com/example/robovision/MainActivity.java
+++ app/src/main/java/com/example/robovision/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.robovision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
-import org.opencv.android.OpenCVLoader;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //GUI
+    private Button mOpenCVActivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Creating OpenCV instance
-        if (!OpenCVLoader.initDebug())
-            Log.e("OpenCV", "unable to load OpenCV");
-        else
-            Log.d("OpenCV", "OpenCV opened successfully");
+        //Setup button to switch to OpenCV color detector
+        mOpenCVActivityBtn = (Button)findViewById(R.id.opencv_btn);
 
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(bluetoothAdapter == null) {
-            //bluetooth unsupported
-            Log.e("Bluetooth","Bluetooth not supported");
-        }
-        else {
-            Log.d("Bluetooth", "Bluetooth adapter initiated");
-        }
+        mOpenCVActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOpenCVActivity();
+            }
+        });
+
+    }
+
+    private void openOpenCVActivity() {
+        Intent intent = new Intent(this, ColorBlobDetectionActivity.class);
+        startActivity(intent);
     }
 }
